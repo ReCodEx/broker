a41a0a7ef8e46695768e4237326fe745955924ab
Teyras <teyras@gmail.com>
2016-02-23 19:00:34 +0100

    add a queuing test

--- tests/broker.cpp
+++ tests/broker.cpp
@@ -15,6 +15,7 @@ class mock_task_router : public task_router {
 public:
 	MOCK_METHOD1(add_worker, void(task_router::worker_ptr));
 	MOCK_METHOD1(find_worker, task_router::worker_ptr(const task_router::headers_t &));
+	MOCK_METHOD1(find_worker_by_identity, task_router::worker_ptr(const std::string &));
 };
 
 class mock_connection_proxy {
@@ -80,6 +81,7 @@ TEST(broker, worker_init)
 			ClearFlags(), SetFlag(message_origin::WORKER)
 		));
 
+	// A wild worker appeared
 	EXPECT_CALL(*sockets, recv_workers(_, _, _))
 		.InSequence(s2)
 		.WillOnce(DoAll(
@@ -97,6 +99,109 @@ TEST(broker, worker_init)
 			std::pair<std::string, std::string>{"env", "c"},
 			std::pair<std::string, std::string>{"hwgroup", "group_1"}
 		))))))
+		.InSequence(s2);
+
+	EXPECT_CALL(*sockets, poll(_, _, _))
+		.InSequence(s1)
+		.WillOnce(SetArgPointee<2>(true));
+
+	broker_connect<mock_connection_proxy> broker(config, sockets, router);
+	broker.start_brokering();
+}
+
+TEST(broker, queuing)
+{
+	auto config = std::make_shared<NiceMock<mock_broker_config>>();
+	auto sockets = std::make_shared<StrictMock<mock_connection_proxy>>();
+	auto router = std::make_shared<StrictMock<mock_task_router>>();
+
+	std::string client_id = "client_foo";
+	task_router::headers_t headers = {{"env", "c"}};
+	task_router::worker_ptr worker = std::make_shared<struct worker>("identity1", headers);
+
+	EXPECT_CALL(*router, find_worker(Eq(headers)))
+		.WillRepeatedly(Return(worker));
+
+	EXPECT_CALL(*router, find_worker_by_identity(worker->identity))
+		.WillRepeatedly(Return(worker));
+
+	Sequence s1, s2;
+
+	EXPECT_CALL(*sockets, bind(_, _))
+		.InSequence(s1, s2);
+
+	// A request has arrived
+	EXPECT_CALL(*sockets, poll(_, _, _))
+		.InSequence(s1)
+		.WillOnce(DoAll(
+			ClearFlags(), SetFlag(message_origin::CLIENT)
+		));
+
+	EXPECT_CALL(*sockets, recv_clients(_, _, _))
+		.InSequence(s2)
+		.WillOnce(DoAll(
+			SetArgReferee<0>(client_id),
+			SetArgReferee<1>(std::vector<std::string>{
+				"eval",
+				"job1",
+				"env=c",
+				"",
+				"1",
+				"2"
+			})
+		));
+
+	// Let the worker process it
+	EXPECT_CALL(*sockets, send_workers(StrEq(worker->identity), ElementsAre("eval", "job1", "1", "2")))
+		.InSequence(s2);
+
+	EXPECT_CALL(*sockets, send_clients(StrEq(client_id), ElementsAre("accept")))
+		.InSequence(s2);
+
+	// Another request has arrived
+	EXPECT_CALL(*sockets, poll(_, _, _))
+		.InSequence(s1)
+		.WillOnce(DoAll(
+			ClearFlags(), SetFlag(message_origin::CLIENT)
+		));
+
+	EXPECT_CALL(*sockets, recv_clients(_, _, _))
+		.InSequence(s2)
+		.WillOnce(DoAll(
+			SetArgReferee<0>(client_id),
+			SetArgReferee<1>(std::vector<std::string>{
+				"eval",
+				"job2",
+				"env=c",
+				"",
+				"3",
+				"4"
+			})
+		));
+
+	// The worker is busy, but we don't want to keep the client waiting
+	EXPECT_CALL(*sockets, send_clients(StrEq(client_id), ElementsAre("accept")))
+		.InSequence(s2);
+
+	// The worker is finally done
+	EXPECT_CALL(*sockets, poll(_, _, _))
+		.InSequence(s1)
+		.WillOnce(DoAll(
+			ClearFlags(), SetFlag(message_origin::WORKER)
+		));
+
+	EXPECT_CALL(*sockets, recv_workers(_, _, _))
+		.InSequence(s2)
+		.WillOnce(DoAll(
+			SetArgReferee<0>(worker->identity),
+			SetArgReferee<1>(std::vector<std::string>{
+				"done",
+				"job1"
+			})
+		));
+
+	// Give the worker some more work
+	EXPECT_CALL(*sockets, send_workers(worker->identity, ElementsAre("eval", "job2", "3", "4")))
 		.InSequence(s2);
 
 	EXPECT_CALL(*sockets, poll(_, _, _))
