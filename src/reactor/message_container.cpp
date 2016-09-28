31329a536bbf5efa1a83a38ba0efed075ebcbe46
Teyras <teyras@gmail.com>
2016-09-28 22:47:50 +0200

    add message_container constructors

--- src/reactor/message_container.cpp
+++ src/reactor/message_container.cpp
@@ -1,1 +1,10 @@
 #include "message_container.h"
+
+message_container::message_container ()
+{
+}
+
+message_container::message_container (const std::string &key, const std::string &identity, const
+std::vector<std::string> &data): key(key), identity(identity), data(data)
+{
+}
