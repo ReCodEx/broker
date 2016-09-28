1a4e3a2ea6cdfdf78dbb9cf61522731d7cc69c8e
Teyras <teyras@gmail.com>
2016-09-29 00:04:16 +0200

    add message_container equality operator

--- src/reactor/message_container.cpp
+++ src/reactor/message_container.cpp
@@ -1,10 +1,16 @@
 #include "message_container.h"
 
-message_container::message_container ()
+message_container::message_container()
 {
 }
 
-message_container::message_container (const std::string &key, const std::string &identity, const
-std::vector<std::string> &data): key(key), identity(identity), data(data)
+message_container::message_container(
+	const std::string &key, const std::string &identity, const std::vector<std::string> &data)
+	: key(key), identity(identity), data(data)
 {
+}
+
+bool message_container::operator==(const message_container &other)
+{
+	return key == other.key && identity == other.identity && data == other.data;
 }
